0f2329dd19e7dce931114f7469ed41dd13cbe73f
Niels Vandekeybus <progster@gmail.com>
2021-05-25 22:16:06 +0200

    tweak eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+      "browser": false,
+      "es2021": false,
+      "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
